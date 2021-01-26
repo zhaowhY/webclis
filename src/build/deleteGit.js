32e8a0219524ea0be13e3e7e1e3ae6d91044e709
zhaowenhao9 <zhaowenhao14@jd.com>
2021-01-26 17:00:03 +0800

    init

--- src/build/deleteGit.js
+++ src/build/deleteGit.js
@@ -1,9 +1,11 @@
 const execa = require('execa');
 const path = require('path');
+const log = require('../log')
+
 
 // 删除clis下面项目中的.git文件
-console.log('start', 111)
 async function deleteGit() {
+  log.warning('start clear clis .git!')
   const { stdout } = await execa('ls', { cwd: path.join(__dirname, '../clis') });
   const clisName = stdout.split('\n')
 
@@ -14,6 +16,7 @@ async function deleteGit() {
   clisName.forEach(dirName => {
     execa('rm', ['-rf', '.git'], { cwd: path.join(__dirname, '../clis', dirName) });
   })
+  log.success('clear success!')
 }
 
 deleteGit()
